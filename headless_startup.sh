0ec4edfc349f92c5e6d4e0c7598463cd1eac4909
Michael Tauraso (RPi) <mtauraso@gmail.com>
2020-09-08 21:19:43 -0700

    Add process priority and correct users to startup script

--- headless_startup.sh
+++ headless_startup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Put the streamtest program in a named tmux session for the pi user
-# assume we're invoked by root
-#
-# Make this appropriate for rc.local in noobs
-#
-sudo -u pi tmux new-session -d -s chickcam /home/pi/chickcam/streamtest.py
+# assume we're invoked by root this should be runnable from rc.local 
+# in noobs
+sudo -u pi \
+	tmux new-session -d -s chickcam \
+	sudo nice -n -10 \
+	sudo -u pi \
+	"/home/pi/chickcam/streamtest.py" ;
